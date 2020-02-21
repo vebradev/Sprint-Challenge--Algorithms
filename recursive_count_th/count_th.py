30c5aa7e45e8f74272991a93597b37f19a93b1c7
vebradev <justinas@vebra.dev>
2020-02-21 09:08:58 +0000

    implement recursive count of searched string

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,14 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
+    target = "th" # set the search term
+    location = word.find(target)  # returns index of first occurence
+    if word[:len(target)] == target or location > 0:
+        return 1 + count_th(word[location + len(target):]) # move the starting index by length of search term and repeat
+    else:
+        return 0
     
-    # TBC
-    
-    pass
+# print(count_th("thooooothoooooooooothooooothooth")) # => 5
